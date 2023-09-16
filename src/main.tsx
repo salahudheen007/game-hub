c37905d07675ffa2caa0f614f72315f1219f6274
salahukutipuravan@gmail.com <salahukutipuravan@gmail.com>
2023-09-16 13:24:27 +0530

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <App/>
     </ChakraProvider>
   </React.StrictMode>
 );
