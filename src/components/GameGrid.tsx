6dcac26d906e4145b0ce368e8ee4e7890d018a14
salahukutipuravan@gmail.com <salahukutipuravan@gmail.com>
2023-09-17 19:40:49 +0530

    added skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,31 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGame();
+  const { games, error, isLoading } = useGame();
+
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
+      {isLoading && 
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+          spacing={10}
+          padding="20px"
+        >
+          {skeletons.map((skelton) => (
+            <GameCardSkeleton />
+          ))}
+        </SimpleGrid>
+      }
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding='20px'>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={10}
+        padding="20px"
+      >
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,21 +25,25 @@ interface FetchGamesResponse {
 const useGame = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading,setLoading]=useState(false);
+
 
   useEffect(() => {
     const controller=new AbortController();
+    setLoading(true);
     apiClients
       .get<FetchGamesResponse>("/games",{signal:controller.signal})
       .then((res) => {
         setGames(res.data.results);
-        console.log(res.data);
+        setLoading(false);
       })
       .catch((err) => {
         if(err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
       return ()=>controller.abort()
   }, []);
-  return{games,error}
+  return{games,error,isLoading}
 };
 export default useGame;
