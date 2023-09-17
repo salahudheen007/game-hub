cb16070c1574eeef2bf75f37c5db91009a019aa5
salahukutipuravan@gmail.com <salahukutipuravan@gmail.com>
2023-09-17 11:56:28 +0530

    Created Platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,42 @@
+import {
+  FaWindows,
+  FaAndroid,
+  FaLinux,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+} from "react-icons/fa";
+import {MdPhoneIphone} from 'react-icons/md'
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons/lib";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+
+    const iconMap:{[key:string]:IconType} = {
+      pc: FaWindows,
+      playstation: FaPlaystation,
+      xbox: FaXbox,
+      nintendo: SiNintendo,
+      android: FaAndroid,
+      mac:FaApple,
+      ios: MdPhoneIphone,
+      web:BsGlobe,
+      linux:FaLinux
+    };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='gray.500'/>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
